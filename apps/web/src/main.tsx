5dabd3491377ffa5fab3868a2a966dcfacecff37
Tuong Le <tuongaz@gmail.com>
2026-05-19 08:33:06 +1000

    studio: import @seeflow/canvas/style.css (build pipeline live)

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App.tsx';
+import '@seeflow/canvas/style.css';
 import './index.css';
 
 const rootEl = document.getElementById('root');
