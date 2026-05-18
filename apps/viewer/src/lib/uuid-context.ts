b2c90a8cd02871cb88f28f6b7192499a2d4c0302
Tuong Le <tuongaz@gmail.com>
2026-05-18 11:04:06 +1000

    remove viewer

--- apps/viewer/src/lib/uuid-context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const UuidContext = createContext<string>('');
-
-export function useUuid(): string {
-  return useContext(UuidContext);
-}
